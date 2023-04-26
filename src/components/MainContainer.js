647d18a092cc00f4dad380ee8aeb904ada94c4b2
Sunil Devadiga <47885920+sunildevadiga16@users.noreply.github.com>
2023-04-26 14:04:28 +0530

    removed unanted code

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,7 @@ import VideoContainer from './VideoContainer';
 
 const MainContainer = () => {
   return (
-    <div className='col-span-11'>MainContainer
+    <div className='col-span-11'>
         <ButtonList />
         <VideoContainer />
     </div>
